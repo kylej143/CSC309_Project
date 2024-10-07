ff7338a07381714377ab3bb1be08ed3297558686
kylej143 <144349166+kylej143@users.noreply.github.com>
2024-10-06 21:32:05 -0400

    Base for the project

---
+++ run.sh
@@ -0,0 +1,5 @@
+npx prisma generate
+npx prisma migrate dev
+npm run dev
+
+# npx prisma studio  use on seperate console if  you want to see the database on http://localhost:5555/
---
+++ startup.sh
@@ -0,0 +1,1 @@
+npm i prisma @prisma/client @prisma/studio
