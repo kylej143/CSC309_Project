42556e0375439a8f1626ef2c8c27017841eef639
kylej143 <144349166+kylej143@users.noreply.github.com>
2024-10-12 21:52:17 -0400

    api handler -> middle ware

--- scriptorium/pages/api/admin/protected.js
+++ scriptorium/pages/api/admin/protected.js
@@ -3,46 +3,34 @@ import { verifyToken } from "@/utils/auth";
 
 
 export default async function token_handler(req, res) {
+    const Authorization = req.headers.authorization;
 
-    if (req.method === "GET") {
-        const Authorization = req.headers.authorization;
-
-        if (!Authorization) {
-            return res.status(401).json({
-                error: "no token",
-            });
-        }
-
-        let userV = await verifyToken(Authorization)
-
-        if (!userV) {
-            return res.status(401).json({
-                error: "invalid token",
-            });
-        }
-
-        const {userId} = userV
-        const user = await prisma.user.findUnique({
-            where: {
-                id: userId,
-            },
+    if (!Authorization) {
+        return res.status(401).json({
+            error: "no token",
         });
-        if (!user) {
-            return res.status(401).json({
-                error: "invalid token",
-            });
-        }
-
-        const {role} = user
-        if (role === "ADMIN") {
-            return res.status(200).json({message: "this is an admin"})
-        }
-        else {
-            return res.status(403).json({error: "403 Forbidden"})
-        }
-
-
-    } else {
-        return res.status(405).json({ message: "Method not allowed" });
     }
+
+    let userV = await verifyToken(Authorization)
+
+    if (!userV) {
+        return res.status(401).json({
+            error: "invalid token",
+        });
+    }
+
+    const {userId} = userV
+    const user = await prisma.user.findUnique({
+        where: {
+            id: userId,
+        },
+    });
+    if (!user) {
+        return res.status(401).json({
+            error: "invalid token",
+        });
+    }
+
+    const {role} = user
+    return role === "ADMIN"
 }
--- scriptorium/pages/api/user/protected.js
+++ scriptorium/pages/api/user/protected.js
@@ -3,39 +3,28 @@ import { verifyToken } from "@/utils/auth";
 
 
 export default async function token_handler(req, res) {
+    const Authorization = req.headers.authorization;
 
-    if (req.method === "GET") {
-        const Authorization = req.headers.authorization;
-
-        if (!Authorization) {
-            return res.status(401).json({
-                error: "no token",
-            });
-        }
-
-        let userV = await verifyToken(Authorization)
-
-        if (!userV) {
-            return res.status(401).json({
-                error: "invalid token",
-            });
-        }
-
-        const {userId} = userV
-
-        const user = await prisma.user.findUnique({
-            where: {
-                id: userId,
-            },
+    if (!Authorization) {
+        return res.status(401).json({
+            error: "no token",
         });
-        if (!user) {
-            return res.status(401).json({
-                error: "invalid token",
-            });
-        }
-        return res.status(200).json({message: "this is a user"})
-
-    } else {
-        return res.status(405).json({ message: "Method not allowed" });
     }
+
+    let userV = await verifyToken(Authorization)
+
+    if (!userV) {
+        return res.status(401).json({
+            error: "invalid token",
+        });
+    }
+
+    const {userId} = userV
+
+    const user = await prisma.user.findUnique({
+        where: {
+            id: userId,
+        },
+    })
+    return user
 }
