53dbf6b44e9a0cafb930a5a4575f77bb502e9874
kylej143 <144349166+kylej143@users.noreply.github.com>
2024-10-26 18:06:28 -0400

    Password with required strength; documentations

--- scriptorium/pages/api/user/refresh.js
+++ scriptorium/pages/api/user/refresh.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
         const {refreshToken} = req.body
 
         if (!refreshToken) {
-            return res.status(401).json({
+            return res.status(400).json({
                 error: "no token",
             });
         }
