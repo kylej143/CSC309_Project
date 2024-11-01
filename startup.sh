f0b027424b9c854e8c795e2539edda178ff012fe
kylej143 <144349166+kylej143@users.noreply.github.com>
2024-11-01 01:35:07 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,6 +2,8 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
 source ~/.bashrc
 export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install v22.11.0
 
 node -v # should print `v22.11.0`
