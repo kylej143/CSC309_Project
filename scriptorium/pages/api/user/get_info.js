87935ffcb7986483666538813e8308707575946e
kylej143 <144349166+kylej143@users.noreply.github.com>
2024-11-03 13:57:28 -0500

    get info

--- scriptorium/pages/api/user/get_info.js
+++ scriptorium/pages/api/user/get_info.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
         return res.status(403).json({error: "403 Forbidden"})
     }
     return res.status(200).json({"username" : userV.username,
-    "name": userV.name
+    "name": userV.name,
     "email": userV.email,
     "avatar": userV.avatar,
     "phoneNumber": userV.phoneNumber,})
