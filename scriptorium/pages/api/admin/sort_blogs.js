935c076daca3f950f81885029a19661439dc1fe7
Alex <113051612+alx672219@users.noreply.github.com>
2024-11-03 14:37:45 -0500

    Added pagination

--- scriptorium/pages/api/admin/sort_blogs.js
+++ scriptorium/pages/api/admin/sort_blogs.js
@@ -9,10 +9,17 @@ export default async function handler(req, res){
     // 2. sort blogs
     if(req.method === 'GET'){
         if(adminV[0]){
+            let page = Number(req.query.page);
+            const pageSize = 10;
+            
+            if (!page) {
+                page = 1;
+            }
             try{
                 const b = await prisma.blog.findMany({orderBy:{flags: 'desc'}});
                 
-                res.status(201).json({blogs: b.map(bb => ({id: bb.id, title: bb.title, flags: bb.flags}))});
+                const b2 = paginateArray(b, pageSize, page);
+                res.status(201).json({blogs: b2.map(bb => ({id: bb.id, title: bb.title, flags: bb.flags}))});
             }catch(error){
                 return res.status(503).json({error:'error'});
             }
@@ -20,4 +27,20 @@ export default async function handler(req, res){
             return res.status(403).json({error: "403 Forbidden"});
         }
     }
+}
+
+export function paginateArray(arr, pageSize, pageNumber) {
+    // formulas for getting the start and end index of the page that needs to be printed
+    let startIndex = pageSize * (pageNumber - 1);
+    let endIndex = pageSize * pageNumber - 1;
+
+    // check out of bounds
+    if (endIndex + 1 > arr.length) {
+        endIndex = Math.min(endIndex, arr.length - 1);
+    }
+    if (startIndex + 1 > arr.length) {
+        return [];
+    }
+    // return a slice of the array based on indices
+    return arr.slice(startIndex, endIndex + 1);
 }
