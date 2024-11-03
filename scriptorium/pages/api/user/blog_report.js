015bf35af5246e5292f73c5ba640239fdb75c519
Alex <113051612+alx672219@users.noreply.github.com>
2024-11-03 03:01:21 -0500

    Update blog_report.js

--- scriptorium/pages/api/user/blog_report.js
+++ scriptorium/pages/api/user/blog_report.js
@@ -17,12 +17,12 @@ export default async function handler(req, res){
                 const check_blog = await prisma.blog.findUnique({where: { id: bid}});
 
                 if(!check_blog){
-                    return res.status(400).json({error:'Blog does not exist.'});
+                    return res.status(403).json({error:'Blog does not exist.'});
                 }else{
                     const reported = await prisma.blogReport.findUnique({where: {bb: {userID: userID,blogID: bid}}});
 
                     if(reported){
-                        return res.status(400).json({error:'You already reported this blog.'});
+                        return res.status(403).json({error:'You already reported this blog.'});
                     }
 
                     await prisma.blogReport.create({data: {userID: userID, blogID: bid, reason: reason}});
