f0cc05c1c639f6e17009f970df9f565476002a83
kylej <kylejoun23@gmail.com>
2024-11-27 18:14:04 -0500

    minor fix

--- scriptorium/components/Run/index.tsx
+++ scriptorium/components/Run/index.tsx
@@ -43,9 +43,8 @@ export default function Run() {
                        placeholder="Write your stdin here!"
                        value={stdin} onChange={(e) => setStdin(e.target.value)}/>
                 {Boolean(language === "java") ?
-                    <input className="fixed p-3 top-44 left-1/4 right-1/2 h-8 bg-amber-300 placeholder-black"
-                           placeholder="Class name you want to run must be Main"
-                           value={className} onChange={(e) => setClassName(e.target.value)}/>: null}
+                    <p className="fixed font-bold text-center top-44 left-1/4 right-1/2 h-8 bg-amber-300 placeholder-black">
+                        Class name you want to run must be Main</p> : null}
                 <button className="fixed text-center top-44 left-3/4 right-24 h-8 bg-amber-300 hover:bg-amber-900"
                         onClick={RunCode}>Run
                 </button>
